75ecc032f5f60e370248a4ee845e27656b5af9df
Omri <mocd12gmail.com>
2016-02-04 19:48:23 +0200

    settings box finishes, search functionality implementation application finishes and bug fixes

--- js/main.js
+++ js/main.js
@@ -7,40 +7,64 @@ function all(selector) {
 }
 
 var updatePage = function (data) {
-    updateNotification(data.notification);
+    updateNotificationArea(data.notification);
     updateQuickActions(data.quickActions);
     if (localStorage.pageData != "" && localStorage.pageData != null) {
-        updateFolders();
+        updateFolders("quick-reports");
+        updateFolders("my-team-folders");
     } else {
         localStorage.pageData = "";
     }
 };
 
-var updateFolders = function () {
-    var inputs = all(".name ,.url");
+var updateFolders = function (tabName) {
+    var inputs = all("." + tabName + " .name" + ", ." + tabName + " .url");
     for (i = 0; i < inputs.length; i++) {
-        var stringSearch = "quick-reports:" + inputs[i].id + "=";
+        var stringSearch = tabName + ":" + inputs[i].id + "=";
         if (localStorage.pageData.indexOf(stringSearch) != -1) {
             var indexStart = localStorage.pageData.indexOf(stringSearch) + stringSearch.length;
             var indexEnd = localStorage.pageData.indexOf(";", indexStart);
             inputs[i].value = localStorage.pageData.substring(indexStart, indexEnd);
-
         }
     }
-
-    var pattern = "." + "quick-reports"+ " .styled-select-list";
+    var pattern = "." + tabName + " .styled-select-list";
+    var emptyFlag = true;
     $(pattern).innerHTML = "";
     for (var i = 0; i < inputs.length; i++) {
         if (inputs[i].value != null && inputs[i].value != "") {
-            $(pattern).innerHTML = $(pattern).innerHTML + "<li><a href=\"" + inputs[i + 1].value + "\">" + inputs[i].value + "</a></li>"
+            if (i == 0) {
+                $(pattern).innerHTML = $(pattern).innerHTML + "<li>" + inputs[i].value + "</li>";
+                all(pattern + " li")[0].title = inputs[i + 1].value;
+                $("." + $(pattern).parentNode.parentNode.className + " .frame-window").src = inputs[i + 1].value;
+                $("." + $(pattern).parentNode.parentNode.className + " .expand-icon").href = inputs[i + 1].value;
+
+            }
+            $(pattern).innerHTML = $(pattern).innerHTML + "<li>" + inputs[i].value + "</li>";
+            all(pattern + " li")[i / 2 + 1].title = inputs[i + 1].value;
+            emptyFlag = false;
         }
         i++;
     }
+    if (emptyFlag == true) {
+        $(pattern).style.display = "none";
 
-
+        $("." + $(pattern).parentNode.parentNode.className + " .frame-window").src = "";
+        $("." + $(pattern).parentNode.parentNode.className + " .expand-icon").href = "";
+    } else {
+        $(pattern).style.display = "block";
+        var listItems = all(pattern + " li");
+        for (i = 0; i < listItems.length; i++) {
+            listItems[i].addEventListener("click", openIframe);
+        }
+        var settingsDiv = $("." + $(pattern).parentNode.parentNode.className + " .settings");
+        settingsDiv.style.display = "none";
+        settingsDiv.style.height = "0";
+        $("." + tabName + " .settings-icon-wrapper").style.backgroundColor = "transparent";
+        listItems[0].click();
+    }
 };
 
-var updateNotification = function (data) {
+var updateNotificationArea = function (data) {
     if (data !== undefined) {
         $(".notifications").innerHTML = "<p>" + data + "</p>";
     }
@@ -64,7 +88,7 @@ var updateQuickActions = function (quickActions) {
         var actionLists = all(".action-list");
         for (i = 0; i < quickActions.length; i++) {
             for (var j = 0; j < quickActions[i].actions.length; j++) {
-                actionLists[i].innerHTML += "<li><a tabindex=\"" + g + "\" href=\"" + quickActions[i].actions[j].url + "\">" + quickActions[i].actions[j].label + "</a></li>"
+                actionLists[i].innerHTML += "<li><a tabindex=\"" + g + "\" href=\"" + quickActions[i].actions[j].url + "\">" + quickActions[i].actions[j].label + "</a></li>";
                 g++;
                 //if (j+1 == quickActions[i].actions.length) {
                 all(".action-list li >a")[q].addEventListener("blur", changeFocus, false);
@@ -99,15 +123,15 @@ var updateTabs = function (iconList) {
     }
     window.addEventListener("hashchange", changeActiveTab, false);
 
-}
+};
 
 var changeFocus = function (e) {
     this.parentNode.parentNode.style.display = "none";
-}
+};
 
 var changeFocusNav = function (e) {
     this.querySelector(".action-list").style.display = "block";
-}
+};
 
 var changeActiveTab = function (e) {
     var newHash = e.newURL.substring(e.newURL.indexOf("#"));
@@ -118,17 +142,7 @@ var changeActiveTab = function (e) {
     $(newHash).style.display = "block";
     $(".active-tab").className = "";
     $("a[href=\"" + newHash + "\"]").parentNode.className = "active-tab";
-}
-
-/*var validateUrl = function (urlToValidate) {
-    var myRegExp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
-
-    if (!myRegExp.test(urlToValidate)) {
-        return false;
-    } else {
-        return true;
-    }
-}*/
+};
 
 var toggleSettingsDiv = function () {
     var parentClass = this.parentNode.parentNode.parentNode.className;
@@ -137,47 +151,26 @@ var toggleSettingsDiv = function () {
         settingsDiv.style.display = "block";
         settingsDiv.style.height = "36%";
         this.parentNode.style.backgroundColor = "white";
+        $("." + parentClass + " .name").focus();
     }
     else {
         settingsDiv.style.display = "none";
         settingsDiv.style.height = "0";
         this.parentNode.style.backgroundColor = "transparent";
     }
-}
-
-var fieldsValidate = function () {
-    var parentClass = this.parentNode.className;
-    var inputs = all("." + parentClass + " .name ," + "." + parentClass + " .url");
-    var validated = true;
-    for (var i = 0; i < inputs.length; i += 2) {
-        if (( inputs[i + 1].value != null && inputs[i + 1].value != "") || ( inputs[i].value != null && inputs[i].value != "")) {
-            inputs[i].required = true;
-            inputs[i + 1].required = true;
-            validated= false;
-        }
-        if (( inputs[i + 1].value == null || inputs[i + 1].value == "") && ( inputs[i].value == null || inputs[i].value == "")) {
-            inputs[i].required = false;
-            inputs[i + 1].required = false;
-        }
-    }
-    /*if (!this.checkValidity()) {
-     $("." + this.parentNode.parentNode.className + " .save").click();
-     }*/
-
-    return validated;
-}
+};
 
 var savePress = function () {
-    var parentClass = this.parentNode.parentNode.className;
-    var inputs = all("." + parentClass + " .name ," + "." + parentClass + " .url");
 
+    var parentClass = this.parentNode.parentNode.parentNode.parentNode.className;
+    var inputs = all("." + parentClass + " .name ," + "." + parentClass + " .url");
     var validated = true;
     for (var i = 0; i < inputs.length; i += 2) {
         if (( inputs[i + 1].value != null && inputs[i + 1].value != "") || ( inputs[i].value != null && inputs[i].value != "")) {
             inputs[i].required = true;
             inputs[i + 1].required = true;
             validated = false;
-            if(( inputs[i + 1].value != null && inputs[i + 1].value != "") && ( inputs[i].value != null && inputs[i].value != "")) {
+            if (( inputs[i + 1].value != null && inputs[i + 1].value != "") && ( inputs[i].value != null && inputs[i].value != "")) {
                 validated = true;
             }
         }
@@ -187,8 +180,8 @@ var savePress = function () {
             validated = true;
         }
     }
-    if (validated == true && this.parentNode.checkValidity()) {
-        parentClass = this.parentNode.parentNode.parentNode.id;
+    if (validated == true && this.parentNode.parentNode.checkValidity()) {
+        parentClass = this.parentNode.parentNode.parentNode.parentNode.id;
         var pattern = parentClass + ":.+?;";
         var regexp = new RegExp(pattern, "g");
         localStorage.pageData = localStorage.pageData.replace(regexp, "");
@@ -197,33 +190,84 @@ var savePress = function () {
                 localStorage.pageData = localStorage.pageData + parentClass + ":" + inputs[i].id + "=" + inputs[i].value + ";";
             }
         }
-        pattern = "." + this.parentNode.parentNode.parentNode.className + " .styled-select-list";
+        pattern = "." + this.parentNode.parentNode.parentNode.parentNode.className + " .styled-select-list";
+        var emptyFlag = true;
         $(pattern).innerHTML = "";
-        $(pattern).style.display="none";
         for (var i = 0; i < inputs.length; i++) {
             if (inputs[i].value != null && inputs[i].value != "") {
-                $(pattern).innerHTML = $(pattern).innerHTML + "<li><a href=\"" + inputs[i + 1].value + "\">" + inputs[i].value + "</a></li>"
+                if (i == 0) {
+                    $(pattern).innerHTML = $(pattern).innerHTML + "<li>" + inputs[i].value + "</li>";
+                    all(pattern + " li")[0].title = inputs[i + 1].value;
+                }
+                $(pattern).innerHTML = $(pattern).innerHTML + "<li>" + inputs[i].value + "</li>";
+                all(pattern + " li")[i / 2 + 1].title = inputs[i + 1].value;
+                emptyFlag = false;
             }
             i++;
         }
-        $("." + this.parentNode.parentNode.parentNode.className + " .settings-icon").click();
+        if (emptyFlag == true) {
+            $(pattern).style.display = "none";
+            $("." + this.parentNode.parentNode.parentNode.parentNode.className + " .frame-window").src = "";
+            $("." + this.parentNode.parentNode.parentNode.parentNode.className + " .expand-icon").href = "";
+        } else {
+            $(pattern).style.display = "block";
+            var listItems = all(pattern + " li");
+            for (i = 0; i < listItems.length; i++) {
+                listItems[i].addEventListener("click", openIframe);
+            }
+        }
+        $("." + this.parentNode.parentNode.parentNode.parentNode.className + " .settings-icon").click();
+        all(pattern + " li")[0].click();
     }
 };
 
+var openIframe = function () {
+    $("." + this.parentNode.parentNode.parentNode.className + " .frame-window").src = this.title;
+    $("." + this.parentNode.parentNode.parentNode.className + " .expand-icon").href = this.title;
+    this.parentNode.parentNode.querySelector("li").title = this.title;
+    this.parentNode.parentNode.querySelector("li").innerHTML = this.innerHTML;
+    this.parentNode.parentNode.querySelector("li").addEventListener("click", openIframe);
+};
+
 var cancelPress = function () {
-    var parentClass = this.parentNode.parentNode.className;
+    var parentClass = this.parentNode.parentNode.parentNode.parentNode.className;
     var inputsList = all("." + parentClass + " .url ," + "." + parentClass + " .name");
     for (var i = 0; i < inputsList.length; ++i) {
         inputsList[i].value = "";
     }
-
-    var settingsDiv = $("." + parentClass);
+    var settingsDiv = $("." + parentClass + " .settings");
     settingsDiv.style.display = "none";
     settingsDiv.style.height = "0";
-    this.parentNode.parentNode.parentNode.querySelector(".settings-icon-wrapper").style.backgroundColor = "transparent";
+    $("." + parentClass + " .settings-icon-wrapper").style.backgroundColor = "transparent";
+    updateFolders(parentClass);
+};
 
-    updateFolders();
-}
+
+var searchEnter = function (e) {
+    var keynum = e.which;
+    if (keynum == 13) {
+        var dropDownList = all(".styled-select-list li");
+        for (var i = 0; i < dropDownList.length; i++) {
+            if (dropDownList[i].innerHTML == this.value) {
+                dropDownList[i].click();
+                i = dropDownList.length + 1;
+            }
+        }
+        if (i == dropDownList.length) {
+            $(".notifications").innerHTML = "<p>" + "The searched report " + this.value + " was not found" + "</p>";
+        }
+    }
+};
+
+var inputKeyPress = function (e) {
+    var keynum = e.which;
+    if (keynum == 0) {
+        var className = this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.className;
+        $("." + className + " .cancel").click();
+    } else if (keynum == 13) {
+        $("." + className + " .save").click();
+    }
+};
 
 function initialize() {
     UTILS.ajax("data/config.json", {done: updatePage});
@@ -233,17 +277,18 @@ function initialize() {
         sButtons[i].addEventListener("click", toggleSettingsDiv);
     }
     sButtons = all(".cancel");
-    for (var i = 0; i < sButtons.length; ++i) {
+    for (i = 0; i < sButtons.length; ++i) {
         sButtons[i].addEventListener("click", cancelPress);
     }
     sButtons = all(".save");
-    for (var i = 0; i < sButtons.length; ++i) {
+    for (i = 0; i < sButtons.length; ++i) {
         sButtons[i].addEventListener("click", savePress);
     }
- /*   var forms = all(".settings > form");
-    for (var i = 0; i < forms.length; ++i) {
-        forms[i].addEventListener("keyup", fieldsValidate);
-    }*/
+    var inputs = all(".name , .url");
+    for (i = 0; i < inputs.length; ++i) {
+        inputs[i].addEventListener("keypress", inputKeyPress);
+    }
+    $(".input-search").addEventListener("keypress", searchEnter);
 }
 
 window.onLoad = initialize();
